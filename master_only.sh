c4517166be37b397a8784ff764863aba3ebcfa41
bbiiggppiigg <bbiiggppiigg@gmail.com>
2017-09-21 00:29:27 -0500

    add new scripts

---
+++ master_only.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+cd /home/ubuntu/software
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT.tar.gz
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT-minimal.tar.gz
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/hive-1.2.1.tar.gz
+
+sudo apt-get install mysql-server
+
--- setup.sh
+++ setup.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+
+mv ./conf.tar.gz ../
+
 sudo apt-get update --fix-missing
 sudo apt-get install openjdk-7-jdk
 sudo apt-get install pdsh
@@ -25,6 +28,21 @@ mkdir -p /home/ubuntu/storage/hdfs/hdfs_nn_dirs
 mkdir -p /home/ubuntu/storage/data/spark/rdds_shuffle
 mkdir -p /home/ubuntu/storage/data/spark/worker
 
+
 cd ~
-wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/conf.tar.gz
 wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/run.sh
+tar -zxvf conf.tar.gz
+
+
+# setup hadoop
+cd /home/ubuntu/software
+wget https://archive.apache.org/dist/hadoop/common/hadoop-2.6.0/hadoop-2.6.0.tar.gz
+tar -zxvf hadoop-2.6.0.tar.gz
+
+#setup spark
+wget  http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/spark-2.0.0-bin-hadoop2.6.tgz
+tar -zxvf spark-2.0.0-bin-hadoop2.6.tgz
+
+
+
+
