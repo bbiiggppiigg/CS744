816ca2e048ae2d792493f875cc828b6140fc5060
bbiiggppiigg <bbiiggppiigg@gmail.com>
2017-09-23 21:05:09 -0500

    fix tez setup

--- master_only.sh
+++ master_only.sh
@@ -1,8 +1,12 @@
 #!bin/bash
 cd /home/ubuntu/software
-wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT.tar.gz
-wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT-minimal.tar.gz
-wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/hive-1.2.1.tar.gz
 
 sudo apt-get install mysql-server
 
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT.tar.gz
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/hive-1.2.1.tar.gz
+
+mkdir tez-0.7.1-SNAPSHOT-minimal
+cd tez-0.7.1-SNAPSHOT-minimal
+wget http://pages.cs.wisc.edu/~akella/CS744/F17/assignment1/tez-0.7.1-SNAPSHOT-minimal.tar.gz
+tar -zxvf tez-0.7.1-SNAPSHOT-minimal.tar.gz
